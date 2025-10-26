6d57f8c2f4df787b8a51c647e16e8a2a94ee3f34
Ron Asnahon <asnahonron@gmail.com>
2025-10-26 14:00:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "00372e1c-0b9a-4b1c-95c8-be75a909cbdd-00-bd9b44wsy10f.spock.replit.dev",
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
